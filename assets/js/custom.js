8f1e784b62dd7e9d35176badcb84521d8bf6c553
admin <saadvavat@gmail.com>
2024-03-17 13:08:09 +0530

    sliding image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -233,3 +233,26 @@ window.addEventListener('load', () => {
     toggleWhatsAppButton(); // Initially show the button
     setInterval(toggleWhatsAppButton, 3000); // Toggle the button every 3 seconds
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let images = document.querySelectorAll('.sliding-background img');
+    let currentImageIndex = 0;
+
+    function showImage(index) {
+        images.forEach((image, i) => {
+            if (i === index) {
+                image.classList.add('active');
+            } else {
+                image.classList.remove('active');
+            }
+        });
+    }
+
+    function nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        showImage(currentImageIndex);
+    }
+
+    // Change image every 5 seconds
+    setInterval(nextImage, 5000);
+});
